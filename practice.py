53361cf2204bff1f4acfe21445fa3a43973e85f6
md sajid ahsan seyam <129988169+codeNina42@users.noreply.github.com>
2025-10-05 22:19:15 +0600

    Update practice.py

--- practice.py
+++ practice.py
@@ -113,7 +113,39 @@ print(t)
 a=int(input("enter number 1 :"))
 b=int(input ("enter number 2 : "))
 
+#---------------------------string _----------------------------
+name="seyam"
+print(name[0:3])
+print(name[-4:-1]) #-5,-4,-3,-2,-1,0
+print(name[1:3])
+print(name[0:5])
+print(name[1:])
+print(name[0:])
+word="abcdefghijklmnopqrstuvwxyx"
+print(word[1:9:4])
+name="seyam are"
+print(len(name))
+print(name.endswith("yama"))#ends with
+print(name.startswith("sey"))#start with
+print(name.capitalize())#capitalize the first latter
+print(name.lower())#lower case
+print(name.upper())#upper case
+print(name.title())#title
+print(name.strip())#cut the space inside the collon
+print(name.replace("seyam","snake"))
+print(name.split())
+words = ["Seyam", "is", "learning", "Python"]
+sentence = " ".join(words)
+print(sentence)#join the substring
+
+print(name.find("a"))#same
+print(name.find("e"))#find the alphabatic index
+print(name.count("e"))#count the alphabet happend
+print(name.startswith("sey"))
+
+
 print("number is :" , a)
 print("number is: " ,b)
 print ("sum is :" ,a+b)
+
 #alhamdulillah xhapter 2 is complete
